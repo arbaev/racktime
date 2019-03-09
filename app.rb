2e27756c5542ccc8a1d8d239ede8a2056a7bdcb0
Tim Arbaev <timbrk@gmail.com>
2019-03-09 16:59:00 +0300

    refactoring

--- app.rb
+++ app.rb
@@ -14,28 +14,25 @@ class App
 
     return wrong_formats_response(ftime.invalid_formats) unless ftime.valid?
 
-    response(200, text_plain, ftime.formatted_time)
+    response(200, ftime.formatted_time)
   end
 
   private
 
-  def response(status, headers, body)
+  def response(status, headers = { 'Content-type' => 'text/plain' }, body)
     [status, headers, ["#{body}\n"]]
   end
 
-  def text_plain
-    { 'Content-type' => 'text/plain' }
-  end
-
   def wrong_path_response
-    response(404, text_plain, "Wrong path, nothing here. Try /time")
+    response 404, "Wrong path, nothing here. Try /time"
   end
 
   def wrong_query_response
-    response(400, text_plain, "Wrong parameter. Try ?format=")
+    response 400, "Wrong parameter. Try ?format="
   end
 
   def wrong_formats_response(formats)
-    response(400, text_plain, "Unknown time format #{formats}. Available formats are: #{Formatter::TIME_FORMATS.keys.join(', ')}")
+    response 400, "Unknown time format #{formats}.
+Available formats are: #{Formatter::TIME_FORMATS.keys.join(', ')}"
   end
 end
